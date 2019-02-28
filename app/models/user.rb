b56ffbaa76ac12239709826314859f3f7079acf4
TonyInfinity <quachhengtony@gmail.com>
2019-02-28 14:13:04 +0000

    Intergrated user roles(student, employer).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: {student: 0, employer: 1}
   belongs_to :user
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :jobs
-
   JOB_TYPES = ["Toàn thời gian", "Bán thời gian", "Hợp đồng", "Tự do", "Thực tập"]
-
   JOB_LOCATIONS = ["An Giang", "Bà Rịa Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước", "Bình Thuận", "Cà Mau", "Cao Bằng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đòng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Tĩnh", "Hải Dương", "Hậu GIang", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Quảng Bình", "Quãng Nam", "Quãng Ngãi", "Quãng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái", "Phú Yên", "Cần Thơ", "Đà Nẵng", "Hải Phòng", "Hà Nội", "Hồ Chí Minh"]
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227093635) do
+ActiveRecord::Schema.define(version: 20190228140243) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20190227093635) do
     t.string   "card_exp_year"
     t.datetime "expires_at"
     t.boolean  "admin"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
